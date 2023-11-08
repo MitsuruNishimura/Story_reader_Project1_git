d5576aace94d6ce944783e19732f7ee7eaed2f07
Mitsuru_Nishimura <mn_thunderbird@outlook.com>
2023-11-08 18:51:59 +0900

    手順 2-3 を確認

---
+++ Story_reader_Project1/MyApp1/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+from datetime import datetime
+
+def index(request):
+    now = datetime.now()
+
+    html_content = "<html><head><title>Hello, Django</title></head><body>"
+    html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+    html_content += "</body></html>"
+
+    return HttpResponse(html_content)
--- Story_reader_Project1/Story_reader_Project1/urls.py
+++ Story_reader_Project1/Story_reader_Project1/urls.py
@@ -24,3 +24,12 @@ urlpatterns = [
     # Uncomment the next line to enable the admin:
     #path('admin/', admin.site.urls)
 ]
+
+from django.urls import include, re_path
+import MyApp1.views
+
+# Django processes URL patterns in the order they appear in the array
+urlpatterns = [
+    re_path(r'^$', MyApp1.views.index, name='index'),
+    re_path(r'^home$', MyApp1.views.index, name='home')
+]
